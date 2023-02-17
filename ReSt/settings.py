2a46bbca59747eb3e053603588b5ef0ee331dd88
Amirhossein <amirabdolii@gmail.com>
2023-02-17 14:13:11 +0330

    feat: db created and psql setted up

--- ReSt/settings.py
+++ ReSt/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'ReSt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rest',
+        'USER': 'postgres',
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost', 
     }
 }
 
