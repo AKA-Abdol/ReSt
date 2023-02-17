ab192928ca482b1b777c0e190fbd2e09ec19ad77
Amirhossein <amirabdolii@gmail.com>
2023-02-17 17:28:55 +0330

    feat: models created

--- ReSt/settings.py
+++ ReSt/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'realtors',
+    'listings',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
