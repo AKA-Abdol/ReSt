7f2d4b343c2d72970e5e40c74adf04563e05b829
Amirhossein <amirabdolii@gmail.com>
2023-02-20 00:26:52 +0330

    fix: endpoints works, no best practice happened

--- realty/urls.py
+++ realty/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:id>/', views.get_realty, name='realty'),
     path('', views.get_all_realties, name='all'),
     path('create/', views.create_realty, name='create')
 ]
--- realty/views.py
+++ realty/views.py
@@ -3,17 +3,29 @@ from rest_framework.decorators import api_view
 from .models import Realty
 from .serializers import RealtySerializer
 
+
 @api_view(["POST"])
 def create_realty(request):
-    data = request.data 
+    data = request.data
     serializer = RealtySerializer(data=data)
     if serializer.is_valid(raise_exception=True):
         serializer.save()
         return Response(serializer.data)
+
 
 @api_view(["GET"])
 def get_all_realties(request):
     queryset = Realty.objects.all()
     data = RealtySerializer(queryset, many=True).data
     print(f'ins:{data}')
+    return Response(data)
+
+
+@api_view(["GET"])
+def get_realty(request, id):
+    try:
+        instance = Realty.objects.get(id=id)
+    except:
+        return Response({"Error":"id doesn't exist!"})
+    data = RealtySerializer(instance).data
     return Response(data)
